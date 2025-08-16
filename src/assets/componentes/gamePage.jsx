b40af8cf90ef5a1a6273a037ee68ff55997bdf2b
Younes <mehrabtahami@gmail.com>
2025-08-16 18:57:29 +0330

    Complete Design & Animation of header's sound setting button & drawer

--- src/assets/componentes/gamePage.jsx
+++ src/assets/componentes/gamePage.jsx
@@ -1,7 +1,34 @@
+import { useRef } from "react";
 import "../styles/gamePage.css";
-import { Settings } from "lucide-react";
+import { gsap } from "gsap";
+import { Settings, Volume2, VolumeOff } from "lucide-react";
 
 export default function GamePage({ playerInfo }) {
+  const soundDrawerRef = useRef(null);
+
+  function handleOpenSoundDrawer(e) {
+    if (soundDrawerRef.current.style.display === "none") {
+      console.log(e.target);
+      e.target.style.transform = "rotate(-45deg)";
+      soundDrawerRef.current.style.display = "flex";
+      gsap.fromTo(
+        soundDrawerRef.current,
+        { scaleY: 0, opacity: 0, transformOrigin: "top" },
+        { scaleY: 1, opacity: 1, duration: 0.4 }
+      );
+    } else {
+      e.target.style.transform = "rotate(0deg)";
+      gsap.to(soundDrawerRef.current, {
+        scaleY: 0,
+        opacity: 0,
+        duration: 0.35,
+        transformOrigin: "top",
+        onComplete: () => {
+          soundDrawerRef.current.style.display = "none";
+        },
+      });
+    }
+  }
   return (
     <div className="game-container">
       <header>
@@ -11,9 +38,26 @@ export default function GamePage({ playerInfo }) {
           </span>
           <p className="player-name">{playerInfo.name}</p>
         </div>
-        <button className="settings-button btn">
-          <Settings size={36} color="#e2e3dd" />
-        </button>
+        <div className="sound-setting">
+          <button
+            className="sound-drawer-button btn"
+            onClick={(e) => handleOpenSoundDrawer(e)}
+          >
+            <Settings
+              size={36}
+              color="#e2e3dd"
+              className="pointer-events-none"
+            />
+          </button>
+          <ul className="soundlist-container" ref={soundDrawerRef}>
+            <li role="button">
+              Music <Volume2 />
+            </li>
+            <li role="button">
+              Click <VolumeOff color="red" />
+            </li>
+          </ul>
+        </div>
       </header>
       <main className="card-container"></main>
       <div className="score-container"></div>
